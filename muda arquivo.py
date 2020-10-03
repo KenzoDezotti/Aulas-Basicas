08a8cba45d9827f22abbd214d3f3dd035de76147
Artur Kenzo <67805044+KenzoDezotti@users.noreply.github.com>
2020-10-03 14:01:06 -0300

    Update muda arquivo.py

--- muda arquivo.py
+++ muda arquivo.py
@@ -1,11 +1,11 @@
 import pandas
 def escrivao(dados):
-    x=open("C:/Users/Arthur/Desktop/anonymous_privacidade_online.txt",'w+')
+    x=open("online.txt",'w+')
     x.write(dados)
     x.close()
 
 def leitor():
-    x=open("C:/Users/Arthur/Desktop/anonymous_privacidade_online.txt",'r+')
+    x=open("online.txt",'r+')
     print(x.read())
     x.close()
 
