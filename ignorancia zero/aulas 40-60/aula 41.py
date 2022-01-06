6e41cb9de6684e2e9891d4b1d84cf41d9e8879bc
Artur Kenzo <kenzodezotti@gmail.com>
2022-01-06 07:39:59 -0300

    lambda
    aula lambda concluida, falta procurar exercicios

---
+++ ignorancia zero/aulas 40-60/aula 41.py
@@ -0,0 +1,13 @@
+#aula ignorancia zero
+#função Lambda
+
+def soma(a,b):
+    resultado = a + b
+    return resultado
+
+somar = lambda a,b: a + b
+
+print(soma(2,2))
+print(somar(2,2))
+
+#a função lambda cria funções com somente uma linha, bom para se criar funções pequenas
