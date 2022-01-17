c569bdf41c5cf6763ac689342e919c0267743a96
Artur Kenzo <kenzodezotti@gmail.com>
2022-01-16 21:01:19 -0300

    inicio da aula 46
    strings são como tuplas e jet ski são como banhistas.

---
+++ ignorancia zero/aulas 40-60/aula 46.py
@@ -0,0 +1,19 @@
+#aula 46
+#strings como sequencias
+"""
+lista = ["a","r","t",'u','r']
+
+for i in lista:
+    print(i)
+
+for i in "artur":
+    print(i)
+"""
+kenzo = "artur"
+#uma string é nada mais q uma tupla, pois é divisivel e imutavel.
+#logo, posso utiliza-la como uma tupla
+print(kenzo[0:3])
+
+
+
+
