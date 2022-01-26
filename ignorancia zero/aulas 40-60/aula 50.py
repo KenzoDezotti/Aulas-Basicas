754c522a6a787cbae5e08b94706fce9586ea8499
Artur Kenzo <kenzodezotti@gmail.com>
2022-01-26 16:35:51 -0300

    .

---
+++ ignorancia zero/aulas 40-60/aula 50.py
@@ -0,0 +1,15 @@
+"""aprendendo a usar a função find e index e count"""
+
+#.index e .find encontra e te entrega a posição de onde a letra/palavra se encontra na posição da string
+print("python".index("on"))        
+
+missi = "mississipi"
+#.count 
+print(missi.count("a"))
+
+#.replace busca e muda a palavra inicial pela segunda
+missi = missi.replace("ss", "zzz")
+
+print(missi) 
+
+
