f9d2cf6241271bb2637309cc186a1e62625a65a2
Artur Kenzo <kenzodezotti@gmail.com>
2022-02-16 13:01:35 -0300

    assert pt1

---
+++ ignorancia zero/aula 66-74/aula 70.py
@@ -0,0 +1,12 @@
+"""aula 70
+assert"""
+
+#assert serve para fazer os seus proprios erros e ajudar a debugar o codigo
+def elevado(num):
+    pass
+    assert num > 0
+    return num**2
+
+
+elevado(-1)
+
