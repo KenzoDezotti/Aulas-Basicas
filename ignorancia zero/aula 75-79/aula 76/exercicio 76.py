422d549eb428b8495a25699c5158f31d050f5b9d
Artur Kenzo <kenzodezotti@gmail.com>
2022-02-24 12:59:02 -0300

    .

--- ignorancia zero/aula 75-79/aula 76/exercicio 76.py
+++ ignorancia zero/aula 75-79/aula 76/exercicio 76.py
@@ -10,42 +10,48 @@ import dbm
 def AbreSite(url):
     from urllib.request import urlopen
     urlopen(url)
-    
-
+    return True
 
 def DataBase(url):
     data = dbm.open("sites.db","c")
-    data[len(data)+1] = url
+    data[url] = url    
 
 def Escritor():
-    pass
-
-def Leitor():
     site = input('digite o link que deseja cadastrar:  ')
     if AbreSite(site) == True:
-        pass
+        DataBase(site)
+
+def Leitor():
+    data = dbm.open("sites.db","c")
+    if len(data) == 0:
+        print("não tem nenhum site no meu banco de dados")
+    else:
+        print("encontrei esses sites salvos: ")
+        for i in data.keys():
+            print(data[i].decode())
 
 def LeOuEscreve():
     """pergunta se tem mais contatos para serem cadastrados"""
     while True:
         opção = input("Deseja cadastrar um novo link ou ler os cadastrados (C/L)\n").lower()
 
-        if opção.rstrip().startswith('l'):
-            Leitor()
-        elif opção.rstrip().startswith('c'):
-            Escritor()
+        if opção.rstrip().startswith('l') or opção.rstrip().startswith('c'):
+            return opção
         else:
             print("Opção inválida, digite novamente.")
 
 def main():
-    pass
+    print("BEM VINDO AO DATABASE DE SITES DO TUTU")
+    verdade=True
+    while verdade:
+        decisao = LeOuEscreve()
+        if decisao .rstrip().startswith('l'):
+            Leitor()
+        else:
+            Escritor()
+        vontade = input("deseja encerrar o programa? (Sim/Nao):  ").lower()
+        if vontade.rstrip().startswith('s'):
+            verdade = False
 
-
-
-
-
-
-
-
-
+main()
 
