cc3860c3260b4b5b3f69ef6f7bfee79aef336674
Artur Kenzo <kenzodezotti@gmail.com>
2022-03-04 09:50:34 -0300

    .

--- ignorancia zero/aula 75-79/aula 79/aula 79.py
+++ ignorancia zero/aula 75-79/aula 79/aula 79.py
@@ -8,12 +8,17 @@ db = shelve.open("shelve.db")
 db["lista"]=[1,2,3,4,5]
 print(db["lista"])
 
+db.close()
+db = shelve.open("shelve.db",writeback= True)
+#deixa mais lento usando o writeback
+db["lista"].append(6)
 
-
-
-
-
-
+#dá pra fazer:
+x = db["lista"]
+x.append(7)
+db["lista"] = x
+#resultado é o mesmo só que o programa nao reduz a velocidade
+print(db["lista"])
 
 
 
