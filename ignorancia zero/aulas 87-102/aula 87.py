3812565e96b352922c7b06f7274ecffd988448f7
Artur Kenzo <kenzodezotti@gmail.com>
2022-03-06 13:57:07 -0300

    .

---
+++ ignorancia zero/aulas 87-102/aula 87.py
@@ -0,0 +1,39 @@
+'''aula de tkinter I'''
+
+from tkinter import *
+
+#dá nome ao quadro principal
+quadro = Tk()
+
+#dá um tamanho ao quadro
+quadro.geometry("800x600")
+
+quadro.title("primeiro programa pos retorno!")
+
+
+#faz o quadro iniciar 
+# sem isso o programa não abre
+quadro.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ignorancia zero/gerador de senha/gerador de senha.py
@@ -0,0 +1,43 @@
+from tkinter import *
+import random
+import shelve
+
+
+class Arquivo():
+    """tudo relacionado ao arquivo com as senhas"""
+
+    def LeitorDeSenha():
+        """le a senha do site especificado"""
+        pass
+    def EscritorDeSenha():
+        '''cadastra a nova senha com o site desejado'''
+        pass
+
+
+def Decisao():
+    '''pergunta ao usuario se quer ler o arquivo ou se deseja criar alguma senha nova'''
+    pass
+
+
+class CriadorDeSenha():
+    '''cria senha com o tamanho desejado'''
+
+    def Embaralha():
+        '''embaralha letras, numeros e sinais'''
+        pass
+    
+    def Algo():
+        pass
+
+
+
+def Quadro():
+    """cria tudo relacionado ao GUI"""
+    quadro = Tk()
+
+    quadro.mainloop()
+
+
+Quadro()
+
+
